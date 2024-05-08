76d1a02392da8de9dee599e96e17dc8562a76e62
Leonel Bustamante <aleobustamante@gmail.com>
2024-05-08 09:47:50 -0300

    Finished Sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Menu from './components/Sidebar/Menu';
-import HomePage from './components/Pages/HomePage';
-import './App.css'
+import React from "react";
+import Menu from "./components/Sidebar/Menu";
+import "./App.css";
+
 function App() {
-  return (
-    <div className="App content">
-      <Menu />
-      <HomePage />
-    </div>
-  );
+	return (
+		<div className="App content">
+			<Menu />
+		</div>
+	);
 }
 
 export default App;
