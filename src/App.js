e026ac0108a8ee575f84b16c65147f920bd4e21e
Leonel Bustamante <aleobustamante@gmail.com>
2024-05-13 12:22:18 -0300

    Created Profile Page

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import Dashboard from './components/Dashboard/Dashboard';
 import Footer from './components/Footer/Footer';
 import './App.css';
 import { Col, Row } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Profile from './components/Profile/Profile';
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Row>
         {/* Sidebar */}
         <Col sm={12} md={12} lg={2}>
           <Sidebar />
         </Col>
         {/* Dashboard */}
-        <Col sm={9} md={10} lg={10}>
-          <Dashboard />
+        <Col sm={12} md={12} lg={10}>
+          <Router>
+            <Routes>
+              <Route path="/" Component={Dashboard} />
+              <Route path="/perfil" Component={Profile} />
+            </Routes>
+          </Router>
         </Col>
         <Col sm={12} md={12} lg={12}>
           <Footer />
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,52 +1,78 @@
-import React , { useState } from 'react';
-import './Sidebar.css';
-import { HouseDoorFill, BookHalf, Star ,HourglassSplit, PersonFill} from 'react-bootstrap-icons';
-import { Nav, Navbar} from 'react-bootstrap';
+import React, { useState } from "react";
+import "./Sidebar.css";
+import {
+  HouseDoorFill,
+  BookHalf,
+  Star,
+  HourglassSplit,
+  PersonFill,
+} from "react-bootstrap-icons";
+import { Nav, Navbar } from "react-bootstrap";
 const Sidebar = () => {
   const [expanded, setExpanded] = useState(false);
 
   const handleToggle = () => setExpanded(!expanded);
   return (
-    <Navbar expand="lg" className="nav navbar d-flex flex-column p-1" id='sidebar' expanded={expanded}>
-      <Navbar.Brand href="/" className="title-menu d-flex align-items-center mb-4">
-        <BookHalf width="40" height="32"><use xlinkHref="#bootstrap"/></BookHalf>
+    <Navbar
+      expand="lg"
+      className="nav navbar d-flex flex-column p-1"
+      id="sidebar"
+      expanded={expanded}
+    >
+      <Navbar.Brand
+        href="/"
+        className="title-menu d-flex align-items-center mb-4"
+      >
+        <BookHalf width="40" height="32">
+          <use xlinkHref="#bootstrap" />
+        </BookHalf>
         <span className="title fs-4">Directorio APP</span>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggle} />
-      
+
       <Navbar.Collapse className="flex-column mb-auto col-12">
-        <hr className='hr-menu'/>
-        <Nav.Item className='col-lg-12'>
-        <Nav.Link href="#0">
+        <hr className="hr-menu" />
+        <Nav.Item className="col-lg-12">
+          <Nav.Link href="/">
             <HouseDoorFill className="bi pe-none me-2" width="16" height="16" />
             Inicio
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className='col-lg-12'>
-          <Nav.Link href="#1">
+        <Nav.Item className="col-lg-12">
+          <Nav.Link href="/">
             <Star className="bi pe-none me-2" width="16" height="16" />
             Favoritas
           </Nav.Link>
         </Nav.Item>
-        <Nav.Item className='col-lg-12'>
-          <Nav.Link href="#2">
-            <HourglassSplit className="bi pe-none me-2" width="16" height="16" />
+        <Nav.Item className="col-lg-12">
+          <Nav.Link href="/">
+            <HourglassSplit
+              className="bi pe-none me-2"
+              width="16"
+              height="16"
+            />
             Recientes
           </Nav.Link>
         </Nav.Item>
-        <hr className='hr-menu'/>
-        <h5 className='title-sec'>Usuario:</h5>
-        <Nav.Item className='col-lg-12'>
-          <Nav.Link href="#3">
+        <hr className="hr-menu" />
+        <h5 className="title-sec">Usuario:</h5>
+        <Nav.Item className="col-lg-12">
+          <Nav.Link href="perfil">
             <PersonFill className="bi pe-none me-2" width="16" height="16" />
             Perfil
           </Nav.Link>
         </Nav.Item>
       </Navbar.Collapse>
-      <hr className='hr-menu'/>
-      <Nav.Link className='col-12'>
-        <img src="https://github.com/mdo.png" alt="mdo" width="35" height="35" className="rounded-circle m-2" />
-          Nombre
+      <hr className="hr-menu" />
+      <Nav.Link className="col-12">
+        <img
+          src="https://github.com/mdo.png"
+          alt="mdo"
+          width="35"
+          height="35"
+          className="rounded-circle m-2"
+        />
+        Nombre
       </Nav.Link>
     </Navbar>
   );
